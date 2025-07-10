52007079945a9e83dcec65ff4c34295a1ee2a015
GlennIvander <ivander792@gmail.com>
2025-07-10 19:55:54 +0800

    added stimulus, routing, 2:02

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,5 @@ pin "@fortawesome/fontawesome-svg-core", to: "@fortawesome--fontawesome-svg-core
 pin "@fortawesome/free-brands-svg-icons", to: "@fortawesome--free-brands-svg-icons.js" # @6.7.2
 pin "@fortawesome/free-regular-svg-icons", to: "@fortawesome--free-regular-svg-icons.js" # @6.7.2
 pin "@fortawesome/free-solid-svg-icons", to: "@fortawesome--free-solid-svg-icons.js" # @6.7.2
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.200
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.12
